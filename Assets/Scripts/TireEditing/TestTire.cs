8aca92306b88514d5503d5a17c7bb5e068818aaf
BoltManGuy <boltsoft@gmail.com>
2015-02-25 18:35:43 -0500

    added sound stuff

--- Assets/Scripts/TireEditing/TestTire.cs
+++ Assets/Scripts/TireEditing/TestTire.cs
@@ -5,6 +5,12 @@ public class TestTire : MonoBehaviour {
 
 	public string tireType;
 	public int slidersLength; 
+
+	public AudioClip tireSound1;
+	public AudioClip tireSound2;
+	public AudioClip tireSound3;
+	public AudioClip tireSound4;
+	public AudioClip tireSound5;
 	
 	GameObject tire;
 	
@@ -19,6 +25,9 @@ public class TestTire : MonoBehaviour {
 	BoxCollider bop;
 
 	float timeScale = 1f;
+
+	AudioClip[] tireSounds;
+	AudioSource tireSound;
 	
 	// Use this for initialization
 	void Start () {
@@ -26,9 +35,18 @@ public class TestTire : MonoBehaviour {
 		
 		
 		tire = GameObject.FindGameObjectWithTag ("MainTire");
-		meshRenderer = tire.GetComponent<SkinnedMeshRenderer> ();
+		meshRenderer = tire.GetComponent<SkinnedMeshRenderer>();
 		meshCollider = tire.GetComponent <MeshCollider>();
+		tireSound = tire.GetComponent<AudioSource>();
 		tireMat = tire.renderer.materials [1];
+
+		tireSounds = new AudioClip[5];
+		tireSounds [0] = tireSound1; 
+		tireSounds [1] = tireSound2; 
+		tireSounds [2] = tireSound3; 
+		tireSounds [3] = tireSound4; 
+		tireSounds [4] = tireSound5; 
+
 		
 		for(int i = 0; i < sliders.Length; i++)
 		{
@@ -71,6 +89,16 @@ public class TestTire : MonoBehaviour {
 			
 		}
 	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+		if (collision.relativeVelocity.magnitude > 2.5f) {
+			tireSound.PlayOneShot (tireSounds [Random.Range (0, tireSounds.Length)], collision.relativeVelocity.magnitude * 0.01f);
+		}
+		}
 	
 	// Update is called once per frame
 	void Update () {
