81e3528c82b2fb6015e6dff00a1b5b7d148e9013
BoltManGuy <boltsoft@gmail.com>
2015-02-24 21:45:39 -0500

    Revert "Revert "New save/load code""
    This reverts commit 2933dc78501203244da6dc67a6c37d2e9b1a5534.

---
+++ Assets/Scripts/TireEditing/SaveLoad.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveLoad : MonoBehaviour {
+
+	public static void Save (string key, float valueToSave) {
+
+		PlayerPrefs.SetFloat(key, valueToSave);
+
+	}
+
+	public static float Load (string key) {
+
+		return PlayerPrefs.GetFloat(key);
+
+	}
+}
