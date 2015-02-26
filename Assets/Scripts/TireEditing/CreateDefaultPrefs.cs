5023a329c97c3e7270b3f6d503e2b8ff67083c4a
BoltManGuy <boltsoft@gmail.com>
2015-02-26 00:21:14 -0500

    Default Prefs added

---
+++ Assets/Scripts/TireEditing/CreateDefaultPrefs.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateDefaultPrefs : MonoBehaviour {
+
+	string tireType = "TestTire";
+
+	// Use this for initialization
+	void Start () {
+		SaveLoad.SaveString (tireType + "_SliderName_" + 0.ToString(), "Skew");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 1.ToString(), "H_R1");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 2.ToString(), "H_R2");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 3.ToString(), "H_R3");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 4.ToString(), "S_R1");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 5.ToString(), "S_R2");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 6.ToString(), "S_R3");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 7.ToString(), "Width");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 8.ToString(), "Diameter");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 9.ToString(), "Roundness");
+		SaveLoad.SaveString (tireType + "_SliderName_" + 10.ToString(), "Rim");
+
+		SaveLoad.SaveInt (tireType + "_SlidersLength" + 10.ToString(), 11);
+	}
+}
