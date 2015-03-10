a27e4bbbbf9172abe453e55fd74c2dd5e8a14881
BoltManGuy <boltsoft@gmail.com>
2015-03-10 07:02:51 -0400

    forgot to do this

--- Assets/Scripts/UI/ObjectMoveToCursor.cs
+++ Assets/Scripts/UI/ObjectMoveToCursor.cs
@@ -6,7 +6,7 @@ public class ObjectMoveToCursor : MonoBehaviour {
 	public Canvas myCanvas;
 
 	void Start(){
-		//Cursor.visible = false;
+		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
