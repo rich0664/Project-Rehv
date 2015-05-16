d083e0b0e30fc93041c43cbb81603bf906bc4ea8
BoltManGuy <boltsoft@gmail.com>
2015-05-16 08:45:44 -0400

    test

---
+++ Assets/Scripts/Mechanics/Powerups/AutoAssemble.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoAssemble : MonoBehaviour {
+
+	bool leg1;
+	bool leg2;
+	bool leg3;
+
+	Rigidbody bodyRB;
+	Rigidbody legRB1;
+
+	Transform lPoint1;
+	Transform lPoint2;
+	Transform lpoint3;
+
+	void Start () {
+		legRB1 = transform.FindChild ("leg1").GetComponent<Rigidbody>();
+	}
+	
+	void LateUpdate () {
+		if (leg1) {
+
+		} else {
+
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+	}
+
+
+
+
+}
